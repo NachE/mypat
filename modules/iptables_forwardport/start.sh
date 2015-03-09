e0698c19a414c89f6bec5af779a98b0dd9a09ec8
JA Nache <nache.nache@gmail.com>
2015-03-09 14:32:40 +0100

    More info on forward port

--- modules/iptables_forwardport/start.sh
+++ modules/iptables_forwardport/start.sh
@@ -18,13 +18,20 @@ printf 'Private PORT: '
 read -r PRIVATEPORT
 
 $SUDOCOMMAND /sbin/iptables -t nat -I PREROUTING -p tcp -d $PUBLICIP --dport $PUBLICPORT -j DNAT --to-destination $PRIVATEIP:$PRIVATEPORT
-$SUDOCOMMAND /sbin/iptables -I FORWARD -m state -d $PRIVATEIP --state NEW,RELATED,ESTABLISHED -j ACCEPT
-
 if [ $? ];
 then
 	echo "Error?"
-	echo "Command used:  $SUDOCOMMAND /sbin/iptables -t nat -A PREROUTING -p tcp --dport $NEWPORT -j REDIRECT --to-port $ORIGPORT"
-	echo "Listing iptables rules: "
-	$SUDOCOMMAND iptables -t nat -L -n -v
-	read -p "Press a key to continue..."
 fi
+echo "Command used: $SUDOCOMMAND /sbin/iptables -t nat -I PREROUTING -p tcp -d $PUBLICIP --dport $PUBLICPORT -j DNAT --to-destination $PRIVATEIP:$PRIVATEPORT"
+
+$SUDOCOMMAND /sbin/iptables -I FORWARD -m state -d $PRIVATEIP --state NEW,RELATED,ESTABLISHED -j ACCEPT
+if [ $? ];
+then
+	echo "Error?"
+fi
+echo "Command used: $SUDOCOMMAND /sbin/iptables -I FORWARD -m state -d $PRIVATEIP --state NEW,RELATED,ESTABLISHED -j ACCEPT"
+
+echo "Listing iptables rules: "
+$SUDOCOMMAND /sbin/iptables -t nat -L -n -v
+read -p "Press a key to continue..."
+
